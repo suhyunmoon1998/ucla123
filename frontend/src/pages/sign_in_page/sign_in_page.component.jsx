17340a7c097f7811371065c723b25f0847feff69
Soohyun Mun <50057577+suhyunmoon1998@users.noreply.github.com>
2022-11-10 13:57:55 -0800

    modify the sign in

--- frontend/src/pages/sign_in_page/sign_in_page.component.jsx
+++ frontend/src/pages/sign_in_page/sign_in_page.component.jsx
@@ -1,26 +1,44 @@
 import React, { useState } from "react";
-import "./sign_in_page.styles.css"
 
+export const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
-export const SignInPage = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
 
-    // const handleSubmittion = (e) => {
-    //     e.preventDefault();
-    //     console.log(email);
-    // }
-
-    return (
-        <div className="auth-form-container">
-            <form>
-                <label for = "email" > email </label>
-                <input value ={email} type ="email" placeholder="youremail@gmail.com" id = "email" name="email"/>
-                <label for = "password" > password </label>
-                <input value = {password} type ="password" placeholder="********" id = "password" name="password"/>
-                <button type="submit"> SignIn</button>
-            </form>
-            <button> Create an Account </button>
-        </div>
-    )
-}
+  return (
+    <div className="auth-form-container">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <button type="submit">Log In</button>
+      </form>
+      <button
+        className="link-btn"
+        onClick={() => props.onFormSwitch("register")}
+      >
+        Don't have an account? Register here.
+      </button>
+    </div>
+  );
+};
