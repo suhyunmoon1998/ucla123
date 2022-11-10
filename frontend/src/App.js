13e97dfe7e642bfdddc590ce90c07655daa40bca
Soohyun Mun <50057577+suhyunmoon1998@users.noreply.github.com>
2022-11-10 14:15:12 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,40 @@
 import { Routes, Route } from "react-router-dom";
 
+import React, { useState } from "react";
+
 import HomePage from "./pages/home_page/home_page.component";
 import ProfilePage from "./pages/profile_page/profile_page.component";
 
 import NavBar from "./components/nav_bar/nav_bar.component";
+import { Login } from "./pages/sign_in_page/sign_in_page.component";
+import { Register } from "./pages/register_page/register_page.componet";
 
 import "./App.css";
 
 function App() {
+  
+  
+  const [currentForm, setCurrentForm] = useState("login");
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
   return (
     <div className="app">
       <Routes>
         <Route path="*" element={<NavBar />}>
           <Route path="" element={<HomePage />} />
           <Route path="profile" element={<ProfilePage />} />
+          <Route
+            path="login"
+            element={
+              currentForm === "login" ? (
+                <Login onFormSwitch={toggleForm} />
+              ) : (
+                <Register onFormSwitch={toggleForm} />
+              )
+            }
+          ></Route>
         </Route>
       </Routes>
     </div>
