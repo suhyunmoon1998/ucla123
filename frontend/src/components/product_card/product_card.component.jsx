92025aca1f72354833d41d2e655527f7f6b5785f
Guy Genis <guy.genis@gmail.com>
2022-11-23 00:13:08 -0800

    Like button changed to heart, interactive + effect

--- frontend/src/components/product_card/product_card.component.jsx
+++ frontend/src/components/product_card/product_card.component.jsx
@@ -18,9 +18,18 @@ const ProductCard = () => {
         <p className="size">Size: 9</p>
       </div>
       <div className="actions"
-        style={{flexDirection: "row", marginLeft:50, marginRight: 50}}>
+        style={{flexDirection: "row", marginLeft:50, marginRight: 50, marginBottom: 40}}>
         <button className="add-to-cart">Add to cart</button>
-        <button className="like">Like</button>
+        <div className="like">
+          <input type="checkbox" defaultChecked="checked" id="favorite" name="favorite-checkbox" defaultValue="favorite-button" />
+          <label for="favorite" className="container">
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" /></svg>
+            <div className="switch">
+              <span className="option-1"></span>
+              <span className="option-2"></span>
+            </div>
+          </label>
+        </div>
       </div>
     </div>
   );
