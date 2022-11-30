99186bfa23c4e60c1a53d9308f3889b536ee4873
vvkumar2 <52425114+vvkumar2@users.noreply.github.com>
2022-11-30 14:30:01 -0800

    added placeholder items in likes/selling tabs in profile

--- frontend/src/components/all_tabs/likes_tab.component.jsx
+++ frontend/src/components/all_tabs/likes_tab.component.jsx
@@ -4,16 +4,20 @@ import "./tab.styles.css"
 import ProductCard from "../product_card/product_card.component.jsx"
 
 const LikesTab = () => {
+  const liked_items = [
+    { "url": "https://cdn.flightclub.com/750/TEMPLATE/325893/1.jpg", "price": 1300, "size": 6},
+    { "url": "https://cdn.flightclub.com/750/TEMPLATE/299066/1.jpg", "price": 900, "size": 9},
+    { "url": "https://cdn.flightclub.com/750/TEMPLATE/174400/1.jpg", "price": 1100, "size": 10},
+    { "url": "https://cdn.flightclub.com/750/TEMPLATE/336354/1.jpg", "price": 800, "size": 10}
+  ]
   return (
     <div className="LikesTab">
     {/* Likes tab content will go here */}
       <div className="tab-contents">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-        </div>
+        {liked_items.map((item) => {
+            return <ProductCard image_url={item.url} price={item.price} size={item.size}/>
+        })}
+      </div>
     </div>
   );
 };
--- frontend/src/components/all_tabs/selling_tab.component.jsx
+++ frontend/src/components/all_tabs/selling_tab.component.jsx
@@ -4,14 +4,18 @@ import "./tab.styles.css"
 import ProductCard from "../product_card/product_card.component.jsx"
 
 const SellingTab = () => {
+  const selling_items = [
+    { "url": "https://cdn.flightclub.com/750/TEMPLATE/307016/1.jpg", "price": 700, "size": 13},
+    { "url": "https://cdn.flightclub.com/750/TEMPLATE/320361/1.jpg", "price": 900, "size": 8},
+    { "url": "https://cdn.flightclub.com/750/TEMPLATE/315865/1.jpg", "price": 1000, "size": 12}
+  ]
   return (
     <div className="LikesTab">
         {/* Selling tab content will go here */}
         <div className="tab-contents">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+          {selling_items.map((item) => {
+            return <ProductCard image_url={item.url} price={item.price} size={item.size}/>
+          })}
         </div>
     </div>
   );
