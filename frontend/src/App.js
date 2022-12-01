d285d33ccadff7012a274995066cd25798123bef
Soohyun Mun <50057577+suhyunmoon1998@users.noreply.github.com>
2022-11-30 18:34:28 -0800

    routing order

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 
 import HomePage from "./pages/home_page/home_page.component";
 import ProfilePage from "./pages/profile_page/profile_page.component";
-
+import F_NavBar from "./components/nav_bar/first_nav_bar.component";
 import NavBar from "./components/nav_bar/nav_bar.component";
 import Register from "./pages/register_page/register_page.component";
 import Login from "./pages/sign_in_page/sign_in_page.component";
@@ -23,10 +23,13 @@ function App() {
   return (
     <div className="app">
       <Routes>
+        <Route path="*" element={<F_NavBar />}>
+          <Route path="" element={<Login />}></Route>
+          <Route path="register" element={<Register />} />
+        </Route>
         <Route path="*" element={<NavBar />}>
-          <Route path="" element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
           <Route path="profile" element={<ProfilePage />} />
-          <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="cart" element={<CartPage />} />
           <Route path="checkout" element={<CheckoutPage />} />
