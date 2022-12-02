4f87b24f971b5c548704705733f9ff545c6e4f90
vvkumar2 <52425114+vvkumar2@users.noreply.github.com>
2022-12-02 00:37:41 -0800

    added homepage filtering functionality

--- frontend/src/pages/home_page/home_page.component.jsx
+++ frontend/src/pages/home_page/home_page.component.jsx
@@ -1,11 +1,49 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import ProductCard from "../../components/product_card/product_card.component";
 
 import "./home_page.styles.css";
+import Dropdown from "../../components/add_products/dropdown.component";
 
 const HomePage = () => {
   const [products, setProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
+  const [filter, setFilter] = useState("")
+  let types= [
+    {
+      value: "",
+      label: "All",
+    },
+    {
+      value: "shoes",
+      label: "Shoes",
+    },
+    {
+      value: "shirt",
+      label: "Shirt",
+    },
+    {
+      value: "pant",
+      label: "Pant",
+    },
+    {
+      value: "jacket",
+      label: "Jacket",
+    },
+  ]
+
+
+  useEffect(() => {
+    const newFilteredProducts = products.filter((product) => {
+      return product.type.toLocaleLowerCase().includes(filter)
+    });
+    
+    setFilteredProducts(newFilteredProducts)
+  }, [products, filter])
+
+  function changeFilter (event) {
+    setFilter(event.value)
+  }
 
   const getProducts = async() =>{
     await axios
@@ -14,13 +52,19 @@ const HomePage = () => {
   }
 
   getProducts();
-//  console.log(products)
   
   return (
     <div className="home-page">
       <h1>Home Page</h1>
+      <div className="filter-container" style={{width: 250}}>
+        <Dropdown
+          placeHolder="Filter by..."
+          options={types}
+          onChange= {changeFilter}
+        />
+      </div>
       <div className="products">
-        {products.map((product, index) => {
+        {filteredProducts.map((product, index) => {
             return <ProductCard key={index} name={product.title} condition={product.condition} image_url={product.image} price={product.price} size={product.size}/>
         })}
       </div>
